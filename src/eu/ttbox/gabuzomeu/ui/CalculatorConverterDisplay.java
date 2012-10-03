207d29d08824601c9f1eade592fa78f48ac0f78e
jmorille <jmorille@generali.fr>
2012-10-03 18:08:09 +0200

    dd

---
+++ src/eu/ttbox/gabuzomeu/ui/CalculatorConverterDisplay.java
@@ -0,0 +1,21 @@
+package eu.ttbox.gabuzomeu.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class CalculatorConverterDisplay extends LinearLayout  {
+
+	public CalculatorConverterDisplay(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle); 
+	}
+
+	public CalculatorConverterDisplay(Context context, AttributeSet attrs) {
+		super(context, attrs); 
+	}
+
+	public CalculatorConverterDisplay(Context context) {
+		super(context); 
+	}
+
+}
