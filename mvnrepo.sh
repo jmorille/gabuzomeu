f67dc5882a98e1058e97841eaf723ad99d94282c
Vincent Dupain <vdupain@gmail.com>
2012-10-03 07:57:52 +0200

    Using /home/vince

--- mvnrepo.sh
+++ mvnrepo.sh
@@ -4,7 +4,7 @@ echo ### Begin updated local Repository ###
 echo ######################################
 
 echo ## Tomcat jdbc pool
-mvn deploy:deploy-file -Durl=file:/Users/jmorille/.m2/repository -Dpackaging=jar -DgroupId=org.javia.arity  -DartifactId=arity -Dversion=2.1.2 -Dfile=maven-repo/arity-2.1.2.jar
+mvn deploy:deploy-file -Durl=file:$HOME/.m2/repository -Dpackaging=jar -DgroupId=org.javia.arity  -DartifactId=arity -Dversion=2.1.2 -Dfile=maven-repo/arity-2.1.2.jar
 
 
 
